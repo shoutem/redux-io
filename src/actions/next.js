69f7cfae9fd5bcdd3b159f8d03018168a6fd9cb6
MrBr <lbracano@gmail.com>
2016-07-25 17:00:08 +0200

    Update next with no next link
    - Return rejected promise if no next link

--- src/actions/next.js
+++ src/actions/next.js
@@ -1,8 +1,12 @@
 import { getCollectionLink, APPEND_MODE } from '../reducers/collection';
 import { getStatus } from '../status';
 import find from './find';
+
 export default (collection, appendMode = true) => {
   const nextLink = getCollectionLink(collection, 'next');
+  if (!nextLink) {
+    return new Promise((resolve, reject) => reject('No next!'));
+  }
   const { schema, tag } = getStatus(collection);
   const findConfig = {
     request: {
