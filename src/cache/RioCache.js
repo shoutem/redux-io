94e13247d3933c8c20d61a172e9a4ae71cabd343
MrBr <lbracano@gmail.com>
2016-09-22 18:19:25 +0200

    Implement isSingleRelationshipModified
    - Add tests

--- src/cache/RioCache.js
+++ src/cache/RioCache.js
@@ -168,6 +168,25 @@ export default class RioCache {
   }
 
   /**
+   * Single relationship is changed if
+   *  there was no relationship and now there is
+   *  relationship item it self is changed
+   *  related to different item then before
+   *
+   * @param relationship
+   * @param cachedRelationship
+   * @returns {boolean}
+   */
+  isSingleRelationshipModified(relationship, cachedRelationship) {
+    if (!relationship) {
+      return relationship !== cachedRelationship;
+    }
+
+    const relationshipItem = this.getValidItem(relationship);
+    return !relationshipItem || relationshipItem !== cachedRelationship;
+  }
+
+  /**
    * Takes collection of item descriptors and check if cached collection items match current items
    *
    * @param descriptorCollection
@@ -206,7 +225,7 @@ export default class RioCache {
     const cachedRelationship = cachedItem[relationshipName];
 
     if (isSingleRelation(relationship)) {
-      return !this.getValidItem(relationship);
+      return this.isSingleRelationshipModified(relationship, cachedRelationship);
     } else if (isCollection(relationship)) {
       return this.areCollectionItemsChanged(relationship, cachedRelationship);
     }
