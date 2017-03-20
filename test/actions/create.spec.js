71feac74ac9f94ea8b53440cddb2845641d5b690
Mihovil <mihovil.kovacevic@shoutem.com>
2017-03-20 17:32:37 +0100

    Support setting update action body in config

--- test/actions/create.spec.js
+++ test/actions/create.spec.js
@@ -312,6 +312,26 @@ describe('Create action creator', () => {
       .to.not.throw('Item is missing in method argument and in config.body');
   });
 
+  it('uses body from config if the item is missing', () => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: 'Body',
+      endpoint: 'api.test',
+    };
+
+    const schema = 'app.builder';
+    const schemaConfig = {
+      schema,
+      request: config,
+    };
+
+    const action = create(schemaConfig);
+
+    expect(action[RSAA].body).to.equal(config.body);
+  });
+
   it('produces valid storage and collection actions', done => {
     const schema = 'schema_test';
     const expectedPayload = {
