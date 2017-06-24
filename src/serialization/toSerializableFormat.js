9571ee748a0162b8c2711a30f817bebbfaba9962
vivanac <vedran@shoutem.com>
2017-06-24 19:30:29 +0200

    Fix serialization of plain arrays and storage status modification and clone

--- src/serialization/toSerializableFormat.js
+++ src/serialization/toSerializableFormat.js
@@ -32,15 +32,18 @@ function saveStatus(originalState, serializableState) {
  * @returns {*}
  */
 function transformSubstate(originalSubState) {
-  if (_.isPlainObject(originalSubState)) {
-    // eslint-disable-next-line  no-use-before-define
-    return toSerializableFormat(originalSubState);
-  } else if (_.isArray(originalSubState) && originalSubState[STATUS]) {
+  if (_.isArray(originalSubState)) {
+    if (!originalSubState[STATUS]) {
+      return originalSubState;
+    }
     // Transform array into format that reverse transformation expects.
     // JSON.stringify only serialize array items, it does not serialize
     // additional array properties, this is used to save those properties.
     // It is important because RIO adds STATUS property to collections.
     return arrayToObject(originalSubState, { [TYPE_KEY]: ARRAY_TYPE });
+  } else if (_.isPlainObject(originalSubState)) {
+    // eslint-disable-next-line  no-use-before-define
+    return toSerializableFormat(originalSubState);
   }
   return originalSubState;
 }
